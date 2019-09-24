105f7342d088b1fa8b49323c7819a956570e7dc1
Tamara Milovanovic <tamara.milovanovic@maibornwolff.de>
2019-09-24 22:31:27 +0200

    component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Blogpost from './components/blogpost';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Blogpost></Blogpost>
     </div>
   );
 }
