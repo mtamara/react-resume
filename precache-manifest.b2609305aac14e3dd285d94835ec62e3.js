98196d9ec8b6843a804dc73302c9c4fb56a33e18
Tamara Milovanovic <tacca1992@gmail.com>
2020-02-06 16:59:43 +0100

    Updates

---
+++ precache-manifest.b2609305aac14e3dd285d94835ec62e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456cd6fde00a43128db707d82f142cdc",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "6ad3068cec227f6386b3",
+    "url": "/react-resume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "1a6cdf6140b6b42160a0",
+    "url": "/react-resume/static/js/2.0a1290ad.chunk.js"
+  },
+  {
+    "revision": "6ad3068cec227f6386b3",
+    "url": "/react-resume/static/js/main.972b61df.chunk.js"
+  },
+  {
+    "revision": "387c9a81d099e38fc290",
+    "url": "/react-resume/static/js/runtime-main.35596c1f.js"
+  }
+]);
